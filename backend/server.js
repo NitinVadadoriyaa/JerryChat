fbd6507c0a82d2a1d4fa2a32b5c48eb65febe270
NitinVadadoriyaa <155728096+NitinVadadoriyaa@users.noreply.github.com>
2024-01-05 20:15:26 +0530

    Update server.js
    CHANGE ORIGIAN AFTER HOST PROJECT

--- backend/server.js
+++ backend/server.js
@@ -52,7 +52,7 @@ const server = app.listen(PORT, () => {
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000", // frontend connection
+    origin: "https://jerrychat.onrender.com/", // frontend connection
   },
 });
 
