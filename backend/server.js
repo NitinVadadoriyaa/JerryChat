e904fa28223d34f4928f106ad021758cc4cd5361
NitinVadadoriya <vadadoriyanitin55@gmail.com>
2024-01-05 18:28:26 +0530

    deployment set-up is done v:1.0.0

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const { Socket } = require("socket.io");
+const path = require("path");
 
 dotenv.config();
 connectDB(); // must call after dotenv.config()
@@ -20,6 +21,24 @@ app.get("/", (req, res) => {});
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
+
+// --------------------------deployment------------------------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+// --------------------------deployment------------------------------
 
 // Error Handling middlewares
 app.use(notFound);
