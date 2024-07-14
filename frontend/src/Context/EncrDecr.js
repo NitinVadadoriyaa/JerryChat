089aadea8dfeb86645ec34f8829241289e25a1fa
NitinVadadoriya <vadadoriyanitin55@gmail.com>
2024-07-14 15:46:18 +0530

    end to end enc/dec implemented

--- frontend/src/Context/EncrDecr.js
+++ frontend/src/Context/EncrDecr.js
@@ -1,15 +1,15 @@
 import * as CryptoJS from "crypto-js";
 
-const secretKey = process.env.REACT_APP_SECRET_KEY
-  ? process.env.REACT_APP_SECRET_KEY
-  : "12345";
+// const secretKey = process.env.REACT_APP_SECRET_KEY
+//   ? process.env.REACT_APP_SECRET_KEY
+//   : "12345";
 
-export const encrypt = (plainText) => {
+export const encrypt = (plainText, secretKey) => {
   const cipherText = CryptoJS.AES.encrypt(plainText, secretKey).toString();
   return cipherText;
 };
 
-export const decrypt = (cipherText) => {
+export const decrypt = (cipherText, secretKey) => {
   const bytes = CryptoJS.AES.decrypt(cipherText, secretKey);
   const plainText = bytes.toString(CryptoJS.enc.Utf8);
   return plainText;
